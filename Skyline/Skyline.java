dba41dffe024f7ed9d42e9aeb1e306e3ec834825
Daniel Moore <Dan@digitaldocs.net>
2013-04-03 00:09:50 -0400

    sets up foundation for Skyline.java

--- Skyline/Skyline.java
+++ Skyline/Skyline.java
@@ -13,39 +13,61 @@ public class Skyline{
 
 		Scanner inFile = new Scanner(System.in);
 		int main_index;
-		int l;
-		int h;
-		int  r;
-
+		
 		main_index = inFile.nextInt();
 
 		while(main_index != 0){
 
-			ArrayList<building> buildings = new ArrayList<building>();
+			ArrayList<Integer> input_skylines = new ArrayList<Integer>();
 
 			for(int i = 0; i < main_index; i ++){
 
-				buildings.add(new building(inFile.nextInt(), inFile.nextInt(), inFile.nextInt()));
+				for(int j = 0; j < 3; j ++){
+					input_skylines.add(inFile.nextInt());
+				}
 
-				System.out.printf("%d %d %d\n", buildings.get(i).l, buildings.get(i).h, buildings.get(i).r);
+				input_skylines.add(0);
+
 			}//end main for
-
-
+			System.out.println("New Skylines");
+			for(int x = 0, y = input_skylines.size(); x < y; x++){
+				System.out.printf("%d ", input_skylines.get(x));
+			}
+			System.out.println();
 			main_index = inFile.nextInt();
 		}//end main while
 
 	}//end main method
 
+	public ArrayList Skyline_split(ArrayList skyline){
+		int size = skyline.size();
+
+		if(size == 4){
+			return skyline;
+		}
+
+		ArrayList<Integer> skyline_one = new ArrayList<Integer>();
+		ArrayList<Integer> skyline_two = new ArrayList<Integer>();
+		ArrayList<Integer> new_skyline = new ArrayList<Integer>();
+
+		int split = (size / 2) + ((size / 2) % 4) - 1;
+		for(int i = 0; i < split; i ++){
+			skyline_one.add(skyline.get(i));
+		}
+		for(int j = split; j < skyline.size(); j ++){
+			skyline_two.add(skyline.get(j));
+		}
+
+		skyline_one = Skyline_split(skyline_one);
+		skyline_two = Skyline_split(skyline_two);
+		new_skyline = Skyline_merge(skyline_one, skyline_two);
+
+		return new_skyline;
+	}
+
+	public ArrayList Skyline_merge(ArrayList sl1, ArrayList sl2){
+		
+	}
+
 }//end Skyline class
 
-class building{
-	int l;
-	int h;
-	int r;
-
-	public building(int l, int h, int r){
-		this.l = l;
-		this.h = h;
-		this.r = r;
-	}//end building constructor
-}
