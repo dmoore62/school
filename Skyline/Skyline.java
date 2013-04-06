7d7bbee040780c6c4ce2e10cae6e93366388fb84
Daniel Moore <dan@digitaldocs.net>
2013-04-06 16:01:16 -0400

    adds coorect algorithm to skyline.java, incomplete

--- Skyline/Skyline.java
+++ Skyline/Skyline.java
@@ -1,7 +1,7 @@
 /*Skyline.java
 Name: Daniel Moore
 COP 3503
-Assignment 5
+Assignment 4
 4/1/2013*/
 
 import java.io.*;
@@ -67,6 +67,122 @@ public class Skyline{
 
 	public ArrayList Skyline_merge(ArrayList sl1, ArrayList sl2){
 		
+		ArrayList<Integer> = new_skyline = new ArrayList<Integer>();
+		int i_sl1 = 0;
+		int i_sl2 = 0;
+
+
+		while(i_sl1 < sl1.size() && i_sl2 < sl2.size()){
+
+			if(sl1.get(i_sl1).intValue() == sl2.get(i_sl2).intValue()){
+				//the index values are the same
+				if(i_sl1 - 1 < 0 && i_sl2 - 1 < 0){
+					//no lookback exists
+					//take the taller building increment both
+					if(sl1.get(i_sl1 + 1).intValue() > sl2.get(i_sl2 + 1).intValue()){
+						//sl1 is higher
+						new_skyline.add(sl1.get(i_sl1));
+						new_skyline.add(sl1.get(i_sl1 + 1));
+					}else{
+						//sl2 is higher, or same
+						new_skyline.add(sl2.get(i_sl2));
+						new_skyline.add(sl2.get(i_sl2 + 1));
+					}
+					i_sl1 += 2;
+					i_sl2 += 2;
+				}else{
+					//one or more exists
+				}
+
+			}else if(sl1.get(i_sl1).intValue() < sl2.get(i_sl2).intValue()){
+				//the top value should go in
+				if(i_sl2 - 1 < 0){
+					//there is no lookback value
+					//sl1 goes in
+					new_skyline.add(sl1.get(i_sl1));
+					new_skyline.add(sl1.get(i_sl1 + 1));
+					i_sl1 += 2;
+				}else{
+					//there is a lookback value
+					if(sl1.get(i_sl1 + 1).intValue() > sl2.get(i_sl2 - 1).intValue()){
+						//the building is higher
+						//sl1 goes in
+						new_skyline.add(sl1.get(i_sl1));
+						new_skyline.add(sl1.get(i_sl1 + 1));
+					i_sl1 += 2;
+					}else{
+						//the building may be hidden
+						if(i_sl1 - 1 < 0){
+							//no lookback on sl1
+							//hidden, skip it
+							i_sl1 += 2;
+						}else{
+							//find if build is going down
+							if(sl1.get(i_sl1 - 1).intValue() > sl1.get(i_sl1 + 1).intValue() && sl1.get(i_sl1 - 1).intValue() > sl2.get(i_sl2 - 1).intValue()){
+								//building is dropping off
+								new_skyline.add(sl1.get(i_sl1));
+								new_skyline.add(sl2.get(i_sl2 - 1));
+								i_sl1 += 2;
+							}else{
+								//hidden, skip it
+								i_sl1 += 2;
+							}
+						}
+
+					}
+
+				}
+			}else if(sl1.get(i_sl1).intValue() > sl2.get(i_sl2).intValue()){
+				//the bottom value should go in
+				if(i_sl1 - 1 < 0){
+					//there is no lookback value
+					//sl2 goes in
+					new_skyline.add(sl2.get(i_sl2));
+					new_skyline.add(sl2.get(i_sl2 + 1));
+					i_sl2 += 2;
+				}else{
+					//there is a lookback value
+					if(sl2.get(i_sl2 + 1).intValue() > sl1.get(i_sl1 - 1).intValue()){
+						//the building is higher
+						//sl2 goes in
+						new_skyline.add(sl2.get(i_sl2));
+						new_skyline.add(sl2.get(i_sl2 + 1));
+						i_sl2 += 2;
+					}else{
+						//the building may be hidden
+						if(i_sl2 - 1 < 0){
+							//no lookback on sl2
+							//hidden, skip it
+							i_sl2 += 2;
+						}else{
+							//find if build is going down
+							if(sl2.get(i_sl2 - 1).intValue() > sl2.get(i_sl2 + 1).intValue() && sl2.get(i_sl2 - 1).intValue() > sl1.get(i_sl1 - 1).intValue()){
+								//building is dropping off
+								new_skyline.add(sl2.get(i_sl2));
+								new_skyline.add(sl1.get(i_sl1 - 1));
+								i_sl2 += 2;
+							}else{
+								//hidden, skip it
+								i_sl2 += 2;
+							}
+						}
+
+					}
+
+				}
+			}
+
+		}
+		if(i_sl1 > sl1.size()){
+			while(i_sl2 < sl2.size()){
+				new_skyline.add(sl2.get(i_sl2));
+				i_sl2 ++;
+			}
+		}else{
+			while(i_sl1 < sl1.size()){
+				new_skyline.add(sl1.get(i_sl1));
+				i_sl1 ++;
+		}
 	}
 
 }//end Skyline class
