eb62639bf12b11c32a3055c466ec0c39b34a4f88
Daniel Moore <Dan@digitaldocs.net>
2013-04-17 17:20:01 -0400

    adds HW5 files

---
+++ sticks/sticks.java
@@ -0,0 +1,69 @@
+/*Sticks.java
+Daniel Moore
+COP 3503 HW5 Part 1
+4/15/2013*/
+
+import java.util.*;
+
+public class sticks{
+
+	public static void main(String[] args){
+		 int loop;
+		 int length;
+		 int size;
+		 int[] ary;
+		 int[][] memo;
+		 Scanner inFile = new Scanner(System.in);
+
+		 loop = inFile.nextInt();
+
+		 for(int loop_i = 0; loop_i < loop; loop_i++){
+
+		 	length = inFile.nextInt();
+		 	size = inFile.nextInt();
+
+		 	ary = new int[size + 2];
+		 	memo = new int[size + 2][size + 2];
+
+		 	ary[0] = 0;
+		 	for(int i = 1; i < size + 1; i ++){
+		 		ary[i] = inFile.nextInt();
+		 	}
+		 	ary[size + 2] = length;
+
+		 	for(int i = 0; i < size + 2; i ++){
+		 		for(int j = 0; j < size + 2; j ++){
+		 			memo[i][j] = -1;
+		 		}
+		 	}
+
+		 	System.out.println(minCut(ary, memo, 0, size+2));
+
+		 }//end main for
+	}//end main method
+
+	public static int minCut(int[] ary, int[][] memo, int i, int j){
+		int score;
+		int best = 2147483647;
+
+		if(memo[i][j] != -1){
+			return memo[i][j];
+		}
+		else if(j == (i + 1)){
+			memo[i][j] = 0;
+			return 0;
+		}
+		else{
+			for(int k = i + 1; k < j; k ++){
+				score = minCut(ary, memo, i, i+k) + minCut(ary, memo, i+k, j) + (ary[j] - ary[i]);
+				if(score < best){
+					best = score;
+				}
+			}
+			memo[i][j] = best;
+			return best;
+		}
+
+	}//end minCut method
+
+}//end sticks class
---
+++ subseq/countseq.java
@@ -0,0 +1,71 @@
+/*countseq.java
+Daniel Moore
+COP 3503 HW5 Part 2
+4/15/2013*/
+
+import javaj.util.*;
+
+public class countseq{
+
+	public static void main(String[] args){
+		int loop;
+		Scanner inFile = new Scanner(System.in);
+		String word1;
+		String word2;
+		loop = inFile.nextInt();
+
+		for(int loop_i = 0; loop_i < loop; loop_i++){
+
+			word1 = new String();
+			word2 = new String();
+
+			word1 = inFile.next();
+			word2 = inFile.next();
+
+			System.out.println(solve(word2, word1));
+
+		}//end loop for
+
+	}//ens main method
+
+	public static int solve(String t, String s){
+
+		int[][] dp = new int[t.length()][s.length()];
+		if(s.length() < t.length())
+			return 0;
+
+		for(int ti = 0; ti < t.length(); ti ++){
+			for(int si = 0; si < s.length(); si ++){
+
+				if(ti == 0){
+					if(si == 0){
+						if(t.charAt(ti) == s.charAt(si)){
+							dp[ti][si] = 1;
+						}else{
+							dp[ti][si] = 0;
+						}
+					}else{
+						dp[ti][si] = dp[ti][si - 1];
+						if(t.charAt(ti) == s.charAt(si)){
+							dp[ti][si] ++;
+						}
+					}
+				}else{
+					if(si < ti){
+						dp[ti][si] = 0;
+					}else{
+						dp[ti][si] = dp[ti][si - 1];
+						if(t.charAt(ti) == s.charAt(si)){
+							dp[ti][si] += dp[ti - 1][si - 1];
+						}
+					}
+				}
+
+			}//end si for
+		}//end ti for
+
+		return dp[t.length() - 1][s.length() - 1];
+
+	}//end solve method
+
+}//end countseq class
